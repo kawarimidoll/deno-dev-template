4526c231a853509ecd8373c5b74e06abc8f982d7
kawarimidoll <kawarimidoll+git@gmail.com>
2021-07-04 12:13:40 +0900

    add codes for deno deploy

---
+++ server.ts
@@ -0,0 +1,8 @@
+/// <reference path="./deploy.d.ts" />
+
+addEventListener("fetch", (event) => {
+  const response = new Response("Hello World!", {
+    headers: { "content-type": "text/plain" },
+  });
+  event.respondWith(response);
+});
