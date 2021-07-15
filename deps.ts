e2c4d10738e5d27f32206b7a960990dd536f5246
kawarimidoll <kawarimidoll+git@gmail.com>
2021-07-15 16:53:14 +0900

    update dependencies

--- deps.ts
+++ deps.ts
@@ -2,6 +2,6 @@ import {
   assert,
   assertEquals,
   assertThrows,
-} from "https://deno.land/std@0.99.0/testing/asserts.ts";
+} from "https://deno.land/std@0.101.0/testing/asserts.ts";
 
 export { assert, assertEquals, assertThrows };
