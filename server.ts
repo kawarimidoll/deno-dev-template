904e434ac7ed85cd7c770e3e49afa561b263a4b1
kawarimidoll <kawarimidoll+git@gmail.com>
2021-09-05 09:59:43 +0900

    feat: update server.ts for Deno Deploy Beta 2

--- server.ts
+++ server.ts
@@ -1,8 +1,30 @@
 /// <reference path="./deploy.d.ts" />
 
-addEventListener("fetch", (event) => {
-  const response = new Response("Hello World!", {
-    headers: { "content-type": "text/plain" },
+const listener = Deno.listen({ port: 8080 });
+console.log(`HTTP server listening on http://localhost:${listener.addr.port}`);
+
+async function handleConn(conn: Deno.Conn) {
+  const httpConn = Deno.serveHttp(conn);
+  for await (const e of httpConn) {
+    e.respondWith(handler(e.request, conn));
+  }
+}
+
+async function handler(request: Request, conn: Deno.Conn) {
+  if (request.url.includes("favicon")) {
+    return new Response("ok");
+  }
+
+  const readme = await Deno.readTextFile("./README.md");
+
+  return new Response(readme, {
+    headers: {
+      "x-localaddr": `${conn.localAddr.hostname}:${conn.localAddr.port}`,
+      "x-remoteaddr": `${conn.remoteAddr.hostname}:${conn.remoteAddr.port}`,
+    },
   });
-  event.respondWith(response);
-});
+}
+
+for await (const conn of listener) {
+  handleConn(conn);
+}
