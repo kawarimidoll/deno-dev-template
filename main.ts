e072c6825de199341d45e9014b7f310c2f8d19f9
kawarimidoll <kawarimidoll+git@gmail.com>
2021-09-05 10:02:03 +0900

    fix: remove logger.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,1 @@
-import { Logger } from "./logger.ts";
-
-Logger.debug("This log is debug!");
-Logger.info("This log is info!");
-Logger.warning("This log is warning!");
-Logger.error("This log is error!");
+console.log("This is main script!");
