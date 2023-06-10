52a50226beaea65a9b76d860a1a19c6fef965594
kawarimidoll <kawarimidoll+git@gmail.com>
2023-06-10 16:55:37 +0900

    feat: add main scripts

--- main.ts
+++ main.ts
@@ -1,1 +1,8 @@
-console.log("This is main script!");
+export function add(a: number, b: number): number {
+  return a + b;
+}
+
+// Learn more at https://deno.land/manual/examples/module_metadata#concepts
+if (import.meta.main) {
+  console.log("Add 2 + 3 =", add(2, 3));
+}
